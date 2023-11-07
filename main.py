0834f897423e553d8371974bc6465664789a602d
chuaaayou <92513725+IMAYOUNG@users.noreply.github.com>
2023-11-07 16:32:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-def add(a,b):
-    return a+b    
+def add(a, b):
+    return a + b
 
-def sub(a,b):
-    return a-b
+def sub(a, b):
+    return a - b
 
-def mul(a,b):
-    
+def mul(a, b):
+    return a * b
 
+def calculate():
+    res = int(input("Input number: "))
 
-res=int(input())
-while True:
-    
-    operator=input()
-    if(operator=='='):
-        print(res)
-        break
-    num2=int(input())
-    if(operator=='+'):
-        res = add(res,num2)
-    elif(operator=='-'):
-        res = sub(res,num2)
-    elif(operator=='*'):
-        res = mul(res,num2)
+    while True:
+        operator = input("연산자(+, -, *, = 중 하나를 입력하세요): ")
 
+        if operator == '=':
+            print("The result:", res)
+            break
+
+        num2 = int(input("Input number: "))
+
+        if operator == '+':
+            res = add(res, num2)
+        elif operator == '-':
+            res = sub(res, num2)
+        elif operator == '*':
+            res = mul(res, num2)
+
+calculate()
