46d839d5e8e03a3a58caf78ab099487586b5b66a
jys-jeong <94434812+jys-jeong@users.noreply.github.com>
2023-11-10 19:54:27 +0900

    [입출력] easter egg, Type change
    1. 이스터에그 함수 매개변수 타입을 float-> int -> string으로 변환
    2. 이 외에 변수명을 가독성 있게 수정함.

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import sys
+import getpass
 
+def add(num1, num2):
+    return num1 + num2
 
-def add(a, b):
-    return a+b
+def sub(num1, num2):
+    return num1 - num2
 
+def mul(num1, num2):
+    return num1 * num2
 
-def sub(a, b):
-    return a-b
-
-
-def mul(a, b):
-    return a*b
-
-
-def easter_egg(a):
+def easter_egg(number):
     messages = {
         "1212": "I am 정종욱 교수님",
         "429": "I am 김소운",
@@ -24,51 +21,45 @@ def easter_egg(a):
         "725": "I am 정예성"
     }
 
-    if a in messages:
-        print(messages[a])
-        # sys.exit()
-
+    if number in messages:
+        print(messages[number])
 
 def calculator():
     isCorrect = True  # '+', '-', '*' 이외의 연산자가 들어올 경우 False 처리
     intFlag = True # 정수 아닌 수가 들어오면 False
     temp = ""  # 한 가지 연산만 들어왔는지 판단하는 temp 변수
-    res = str(input())
-    if not (str.isdigit(res)):
+    result = str(input())
+    if not (str.isdigit(result)):
         intFlag = False
-    res = float(res)    
+    result = float(result)    
         
     while True:
         operator = input()
 
-        # '=' 입력이 들어오면 계산 종료
-        if (operator == '='):
+        if operator == '=':
             break
 
-        # '+', '-', '*' 이외의 연산자가 들어올 경우 False 처리 + 이전 연산자와 다른 경우 False 처리
         if operator not in ('+', '-', '*') or (temp and temp != operator):
             isCorrect = False
         else:
             temp = operator
 
-        # 가독성을 위해 num2 변수명 --> next_num으로 수정
         next_num = str(input())
         if not (str.isdigit(next_num)):
             intFlag = False
         next_num = float(next_num)
 
-        if (operator == '+'):
-            res = add(res, next_num)
-        elif (operator == '-'):
-            res = sub(res, next_num)
-        elif (operator == '*'):
-            res = mul(res, next_num)
+        if operator == '+':
+            result = add(result, next_num)
+        elif operator == '-':
+            result = sub(result, next_num)
+        elif operator == '*':
+            result = mul(result, next_num)
 
     if isCorrect and intFlag:
-            print(int(res))
-            easter_egg(str(res))
+        print(int(result))
+        easter_egg(str(int(result)))
     else:
-        print("ERROR!!!")
-
+        print("ERROR!")
 
 calculator()
