e022766c6eea5d6f31c8264cc0fa028b1108b807
00onni <78854125+00onni@users.noreply.github.com>
2023-12-05 19:51:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -71,5 +71,49 @@ def calculator():
                 print(int(result))
             else:
                 print("[SYSTEM] ERROR!")
+#아래로 유닛 테스팅용 함수
+def addTest():
+    print("add함수 테스트 결과: ")
+    tc = [(-10, 1), (10, -20), (0, 0)]
+    ans = [-9, -10, 0]
+    
+    for i in range(3):
+        if add(tc[i][0], tc[i][1]) != ans[i]:
+            return False
+    return True
+
+def mulTest():
+    print("mul함수 테스트 결과: ")
+    tc = [(-10, 2), (5, -3), (0, 10)]
+    ans = [-20, -15, 0]
+    
+    for i in range(3):
+        if mul(tc[i][0], tc[i][1]) != ans[i]:
+            return False
+    return True
+
+def easter_eggTest():
+    test_cases = ["1212", "429", "803", "1005", "903", "117", "725", "1015", "1111"]
+    expected_results = {
+        "1212": "I am 정종욱 교수님",
+        "429": "I am 김소운",
+        "803": "I am 황예찬",
+        "1005": "I am 최아영",
+        "903": "I am 정은주",
+        "117": "I am 오주형",
+        "725": "I am 정예성",
+        "1015": "전북대 개교기념일입니다."
+    }
+
+    for case in test_cases:
+        print(f"Testing with input {case}:")
+        easter_egg(case)
+        expected_result = expected_results.get(case, "메시지가 안나와야합니다")
+        print(f"Expected: {expected_result}")
+        print("=" * 30)
+#유닛 테스팅용 함수 끝
+print(addTest())
+print(mulTest())
+print(easter_eggTest())
 
 calculator()
